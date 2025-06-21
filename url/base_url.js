1302ead519e93319f03e091b0210630d3c015782
Shrirang Mohare <mutex7171@gmail.com>
2025-06-21 17:11:18 +0530

    Changed base url

--- url/base_url.js
+++ url/base_url.js
@@ -1,5 +1,5 @@
 const LOCAL = 'http://192.168.1.2:3000'
-const BACKEND = 'https://sms-backend-hjfp.onrender.com'
+const BACKEND = 'https://sms-backend-13ff.onrender.com'
 const PORT_FORWARDING = "http://106.213.84.8:3000"
 const BASE_URL = BACKEND
 
