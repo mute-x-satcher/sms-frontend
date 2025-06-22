d3debb4e62107123bfc64e90e05ddb8b735f4af1
Shrirang Mohare <mutex7171@gmail.com>
2025-06-22 20:35:12 +0530

    base url port issue -minor

--- url/base_url.js
+++ url/base_url.js
@@ -1,6 +1,6 @@
 const LOCAL = 'http://192.168.1.2:3000'
 const BACKEND = 'https://sms-backend-13ff.onrender.com'
 const PORT_FORWARDING = "http://106.213.84.8:3000"
-const BASE_URL = LOCAL
+const BASE_URL = BACKEND
 
 export default BASE_URL
