2f5fc920638388db78c4804438b349b0810f490c
Shrirang Mohare <mutex7171@gmail.com>
2025-06-27 16:36:38 +0530

    checked everything is working

--- components/appendStudents.js
+++ components/appendStudents.js
@@ -94,6 +94,7 @@ mainCard.appendChild(menuContainer)
 const nameDiv = document.createElement('div')
 const rollDiv = document.createElement('div')
 const absentDiv = document.createElement('div')
+const bunkDiv = document.createElement('div')
 const lable = document.createElement('div')
 
 
@@ -101,11 +102,13 @@ const lable = document.createElement('div')
 nameDiv.textContent = `${student.studentName}`
 rollDiv.textContent = `Roll No: ${student.rollNumber}`
 absentDiv.textContent = `Absent Count: ${student.absentCount}`
+bunkDiv.textContent = `Bunk Count: ${student.bunkCount}`
 lable.textContent = 'Parent Contacts:'
 
 mainCard.appendChild(nameDiv)
 mainCard.appendChild(rollDiv)
 mainCard.appendChild(absentDiv)
+mainCard.appendChild(bunkDiv)
 mainCard.appendChild(lable)
 
 student.parentContacts?.map((number,index)=>{
