f988002fbe141c3a4600d8d69756b13cc17d4f1c
Shrirang Mohare <mutex7171@gmail.com>
2025-12-11 15:38:29 +0530

    Final -----><------

--- js/Index.js
+++ js/Index.js
@@ -4,5 +4,6 @@ GuestBtn.addEventListener('click',(e)=>{
     e.preventDefault();
     console.log('sdsd')
     localStorage.setItem('accountId','685586542773bd06b8e5c998')
+    localStorage.setItem('firstVisitor','')
      window.location.href = './pages/homePage.html';
 })
